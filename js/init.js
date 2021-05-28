f78cc4cbae0eea9716cf06bb8ae58cc0ac4d9a0d
cristmtzg <cristhmartg@gmail.com>
2021-05-28 14:03:46 -0500

    login

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyDpfTdINjYOiAV741I1unW_1HYBQzpnsNw",
+    authDomain: "cristmtzg.firebaseapp.com",
+    projectId: "cristmtzg",
+    storageBucket: "cristmtzg.appspot.com",
+    messagingSenderId: "89312578156",
+    appId: "1:89312578156:web:14372d0c3a5e4dbc802649",
+    measurementId: "G-S3Q9T75VWF"
+});
